60cfa5307cab733571ad7340d2fe8e89b6cbd344
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-27 13:22:11 -0400

    Feat: Add Struct, Mappings and Storage Variables

--- src/PersonalRegistry.sol
+++ src/PersonalRegistry.sol
@@ -5,4 +5,17 @@ pragma solidity ^0.8.19;
  * @title PersonalRegistry
  * @dev A simple contract where users can store and update their personal profile information
  */
-contract PersonalRegistry {}
+contract PersonalRegistry {
+    struct Profile {
+        string name;
+        string bio;
+        string website;
+        string twitter;
+        string github;
+        uint256 lastUpdated;
+        bool exists;
+    }
+
+    mapping(address => Profile) public profiles;
+    address[] public registeredUsers;
+}
