ed41ff74e85203ae5f4f95a12181265b55726d76
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-27 13:20:27 -0400

    Feat: Create PersonalRegistry Contract

---
+++ src/PersonalRegistry.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title PersonalRegistry
+ * @dev A simple contract where users can store and update their personal profile information
+ */
+contract PersonalRegistry {}
