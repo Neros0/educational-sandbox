989c0dac59c87d5d968a9f94d6566bfbae71b412
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-27 13:51:00 -0400

    Feat: Create & Configure PersonalRegistry Deployment Script

---
+++ script/DeployPersonalRegistry.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {PersonalRegistry} from "../src/PersonalRegistry.sol";
+
+contract DeployPersonalRegistry is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        // Deploy the PersonalRegistry contract
+        address personalRegistry = address(new PersonalRegistry());
+
+        console2.log("PersonalRegistry deployed at:", personalRegistry);
+
+        vm.stopBroadcast();
+    }
+}
