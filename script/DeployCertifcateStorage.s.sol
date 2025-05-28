467757f5d508cda3330051773203917ac4b48dcb
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-28 11:22:06 -0400

    Feat: Create and Configure CertificateStorage Deployment Script

---
+++ script/DeployCertifcateStorage.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {CertificateStorage} from "../src/CertificateStorage.sol";
+
+contract DeployCertificateStorage is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        // Deploy the CertificateStorage contract
+        address certificateStorage = address(new CertificateStorage());
+
+        console2.log("CertificateStorage deployed at:", certificateStorage);
+
+        vm.stopBroadcast();
+    }
+}
