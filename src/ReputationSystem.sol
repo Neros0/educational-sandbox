d665f3c6cd3fc04e8ff0bb148b9ee7ceec00ffa6
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-28 13:42:19 -0400

    Feat: Create ReputationSystem Contract

---
+++ src/ReputationSystem.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title ReputationSystem
+ * @dev A basic reputation system where users can give/receive endorsements
+ */
+contract ReputationSystem {}
