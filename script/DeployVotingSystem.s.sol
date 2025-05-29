ba91ec5c8593a2bc368a6d55447bebed9b211fed
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-29 13:54:51 -0400

    Feat: Create & Configure VotingSystem Deployment Script

---
+++ script/DeployVotingSystem.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {VotingSystem} from "../src/VotingSystem.sol";
+
+contract DeployVotingSystem is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        // Deploy the VotingSystem contract
+        address votingSystem = address(new VotingSystem());
+
+        console2.log("VotingSystem deployed at:", votingSystem);
+
+        vm.stopBroadcast();
+    }
+}
