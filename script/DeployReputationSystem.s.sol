e95cd2194b24a1470161de92972139bbd8df2513
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-29 10:18:41 -0400

    Feat: Create & Configure ReputationSystem Deployment Script

---
+++ script/DeployReputationSystem.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {ReputationSystem} from "../src/ReputationSystem.sol";
+
+contract DeployReputationSystem is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        // Deploy the ReputationSystem contract
+        address reputationSystem = address(new ReputationSystem());
+
+        console2.log("ReputationSystem deployed at:", reputationSystem);
+
+        vm.stopBroadcast();
+    }
+}
