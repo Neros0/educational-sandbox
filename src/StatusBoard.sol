cb2f81f511dd2d1cabe494fb4c373157068875e5
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-01 00:27:36 -0400

    Feat: Add updateStatus Func

--- src/StatusBoard.sol
+++ src/StatusBoard.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.19;
 
 contract StatusBoard {
     mapping(address => bytes32) public statuses;
+
+    function updateStatus(bytes32 status) external {
+        statuses[msg.sender] = status;
+    }
 }
