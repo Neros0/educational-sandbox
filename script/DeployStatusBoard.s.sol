616bd4e450025fe1439efc9223035f0b16c12160
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-01 00:31:08 -0400

    Feat: Create & Configure StatusBoard Deployment Script

---
+++ script/DeployStatusBoard.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {StatusBoard} from "../src/StatusBoard.sol";
+
+contract DeployStatusBoard is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        // Deploy the StatusBoard contract
+        address statusBoard = address(new StatusBoard());
+
+        console2.log("StatusBoard deployed at:", statusBoard);
+
+        vm.stopBroadcast();
+    }
+}
