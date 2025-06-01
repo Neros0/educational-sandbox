abcc4f366673e13a99903a6e1f0d1adee378544b
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-31 23:57:26 -0400

    Feat: Create & Configure GlobalCounter Deployment Script

---
+++ script/DeployGlobalCounter.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {GlobalCounter} from "../src/GlobalCounter.sol";
+
+contract DeployGlobalCounter is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        // Deploy the GlobalCounter contract
+        address globalCounter = address(new GlobalCounter());
+
+        console2.log("GlobalCounter deployed at:", globalCounter);
+
+        vm.stopBroadcast();
+    }
+}
