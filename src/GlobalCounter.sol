86fd5c3d9eb484c828665d9b4e9d0394427a86fc
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-31 23:47:12 -0400

    Feat: Add Storage Variables

--- src/GlobalCounter.sol
+++ src/GlobalCounter.sol
@@ -2,4 +2,7 @@
 pragma solidity ^0.8.19;
 
 // 1. Global Counter - Anyone can increment, great for teaching state changes
-contract GlobalCounter {}
+contract GlobalCounter {
+    // @notice The number of times this contract has been incremented
+    uint256 public count;
+}
