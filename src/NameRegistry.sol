06a8ae5c087b3665a9d2abea053f111163316ed9
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-01 00:06:35 -0400

    Feat: Add Mappings

--- src/NameRegistry.sol
+++ src/NameRegistry.sol
@@ -2,4 +2,7 @@
 pragma solidity ^0.8.19;
 
 // 2. Simple Name Registry - First come, first served
-contract NameRegistry {}
+contract NameRegistry {
+    mapping(bytes32 => address) public nameToAddress;
+    mapping(address => bytes32) public addressToName;
+}
