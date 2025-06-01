a7486555b5321cbbbbe32e64b028e6bd12a0707b
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-01 12:29:21 -0400

    Feat: Add Mappings and Storage Variables

--- src/SimplePoll.sol
+++ src/SimplePoll.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract SimplePoll {}
+contract SimplePoll {
+    bytes32 public question;
+    mapping(address => bool) public votes; // true = yes, false = no
+    mapping(address => bool) public hasVoted;
+    uint256 public yesCount;
+    uint256 public noCount;
+}
