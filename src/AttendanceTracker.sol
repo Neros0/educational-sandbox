448cb202883b513bccc0b348e74fb7d4cbf2f7ea
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-03 11:13:01 -0400

    Feat: Add markAttendance Func

--- src/AttendanceTracker.sol
+++ src/AttendanceTracker.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.19;
 
 contract AttendanceTracker {
     mapping(bytes32 => mapping(address => bool)) public attendance;
+
+    function markAttendance(bytes32 eventId) external {
+        attendance[eventId][msg.sender] = true;
+    }
 }
