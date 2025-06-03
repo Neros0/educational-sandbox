ced0470d849b494cd278b5752fa0e7bcbbb04ba9
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-03 10:27:13 -0400

    Feat: Add leaveMessage Func

--- src/MessageWall.sol
+++ src/MessageWall.sol
@@ -3,4 +3,9 @@ pragma solidity ^0.8.19;
 
 contract MessageWall {
     mapping(address => mapping(address => bytes32)) public messages;
+
+    // Leave a message for someone
+    function leaveMessage(address recipient, bytes32 message) external {
+        messages[msg.sender][recipient] = message;
+    }
 }
