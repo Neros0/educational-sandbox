cdb7fe913406b46f9d266feda42394d45b4675e3
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-03 11:25:51 -0400

    Feat: Create & Configure AttendanceTracker Deployment Script On Base

---
+++ script/DeployAttendanceTracker.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {AttendanceTracker} from "../src/AttendanceTracker.sol";
+
+contract DeployAttendanceTracker is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        // Deploy the AttendanceTracker contract
+        address attendanceTracker = address(new AttendanceTracker());
+
+        console2.log("AttendanceTracker deployed at:", attendanceTracker);
+
+        vm.stopBroadcast();
+    }
+}
