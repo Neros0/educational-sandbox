fffd651f4e9c7e9030637c436ac18e7d15a4c683
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-04 15:58:04 -0400

    Edit: Update Import Path For PersonalRegistry

--- script/DeployPersonalRegistry.s.sol
+++ script/DeployPersonalRegistry.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 import {Script, console2} from "forge-std/Script.sol";
-import {PersonalRegistry} from "../src/PersonalRegistry.sol";
+import {PersonalRegistry} from "../src/Week 1/PersonalRegistry.sol";
 
 contract DeployPersonalRegistry is Script {
     function run() external {
