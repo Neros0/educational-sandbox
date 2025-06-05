f7c00881d342857d526765facee2905a445da2d4
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-05 14:22:44 -0400

    Feat: Add Mappings And Storage Variables

--- src/AccessControl.sol
+++ src/AccessControl.sol
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract AccessControl {}
+contract AccessControl {
+    mapping(address => uint8) public roles; // 0=none, 1=user, 2=admin
+    address public superAdmin;
+}
