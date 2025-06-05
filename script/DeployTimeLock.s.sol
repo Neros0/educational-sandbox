5501d21c670d4ffd9c788f791a6c20bb2b528506
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-05 14:41:05 -0400

    Feat: Create & Configure TimeLock Deployment Script

---
+++ script/DeployTimeLock.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {TimeLock} from "../src/TimeLock.sol";
+
+contract DeployTimeLock is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        // Deploy the TimeLock contract
+        address timeLock = address(new TimeLock());
+
+        console2.log("TimeLock deployed at:", timeLock);
+
+        vm.stopBroadcast();
+    }
+}
