21f61fe14c6c719976e0db0f92a4f09ca554aaf6
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-05 14:09:06 -0400

    Feat: Add lockFunds Func

--- src/TimeLock.sol
+++ src/TimeLock.sol
@@ -4,4 +4,11 @@ pragma solidity ^0.8.19;
 contract TimeLock {
     mapping(address => uint256) public lockTime;
     mapping(address => uint256) public lockedAmount;
+
+    function lockFunds(uint256 _lockDuration) external payable {
+        require(msg.value >= 0, "Must send a value of ETH");
+
+        lockTime[msg.sender] = block.timestamp + _lockDuration;
+        lockedAmount[msg.sender] = msg.value;
+    }
 }
