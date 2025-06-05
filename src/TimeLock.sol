b99de3f4dcbcece24676b05d52946234dddd1ec5
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-05 14:04:43 -0400

    Feat: Add Mappings

--- src/TimeLock.sol
+++ src/TimeLock.sol
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract TimeLock {}
+contract TimeLock {
+    mapping(address => uint256) public lockTime;
+    mapping(address => uint256) public lockedAmount;
+}
