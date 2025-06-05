5ce5ec4111a65f1ac131334c0988aacb86e99e31
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-05 14:38:58 -0400

    Feat: Create & Configure AccessControl Deployment Script

---
+++ script/DeployAccessControl.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {AccessControl} from "../src/AccessControl.sol";
+
+contract DeployAccessControl is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        // Deploy the AccessControl contract
+        address accessControl = address(new AccessControl());
+
+        console2.log("AccessControl deployed at:", accessControl);
+
+        vm.stopBroadcast();
+    }
+}
