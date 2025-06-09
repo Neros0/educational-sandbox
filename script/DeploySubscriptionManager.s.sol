b019b84da5560360bb10b652c771e8a557403e5a
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-08 22:11:49 -0400

    Feat: Create & Configure SubscriptionManager Deployment Script

---
+++ script/DeploySubscriptionManager.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {SubscriptionManager} from "../src/SubscriptionManager.sol";
+
+contract DeploySubscriptionManager is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        // Deploy the SubscriptionManager contract
+        address subscriptionManager = address(new SubscriptionManager());
+
+        console2.log("SubscriptionManager deployed at:", subscriptionManager);
+
+        vm.stopBroadcast();
+    }
+}
