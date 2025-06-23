45d2d12251c69cd77ec69729d3c109fdff95ee0d
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-23 14:13:28 -0400

    Edit: Refactor /script Folder With Adding Week 4 Folder

--- script/Week 4/DeployEscrowContract.s.sol
+++ script/Week 4/DeployEscrowContract.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 import {Script, console2} from "forge-std/Script.sol";
-import {EscrowContract} from "../src/Week 4/EscrowContract.sol";
+import {EscrowContract} from "../../src/Week 4/EscrowContract.sol";
 
 contract DeployEscrowContract is Script {
     function run() external {
