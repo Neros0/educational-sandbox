3205b1cdd1c5a57200571af957a8dc9341d7147c
Shawn Rizo <shawnrizo@hotmail.com>
2025-07-26 14:15:44 -0400

    Feat: Add attempts mapping

--- src/RiddleHunt7.sol
+++ src/RiddleHunt7.sol
@@ -4,4 +4,6 @@ pragma solidity ^0.8.19;
 contract RiddleHunt7 {
     bytes32 public immutable answerHash;
     string public constant hint = "The answer is a esports team name";
+
+    mapping(address => uint256) public attempts;
 }
