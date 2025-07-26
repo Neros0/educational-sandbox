f8841aac1a67c62d9d325a6fdf06a31678db0cc6
Shawn Rizo <shawnrizo@hotmail.com>
2025-07-26 14:20:19 -0400

    Feat: Add attempts mapping

--- src/RiddleHunt8.sol
+++ src/RiddleHunt8.sol
@@ -4,4 +4,6 @@ pragma solidity ^0.8.19;
 contract RiddleHunt8 {
     bytes32 public immutable answerHash;
     string public constant hint = "The answer is a type of dinosaur";
+
+    mapping(address => uint256) public attempts;
 }
