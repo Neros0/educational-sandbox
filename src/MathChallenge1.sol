c730d9b2693ccaf6692a49dc8617d8368286a53a
Shawn Rizo <shawnrizo@hotmail.com>
2025-07-27 13:41:07 -0400

    Feat: Add Storage Vars

--- src/MathChallenge1.sol
+++ src/MathChallenge1.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract MathChallenge1 {}
+contract MathChallenge1 {
+    string public problem;
+    uint256 public immutable correctAnswer;
+    string public hint;
+    uint256 public difficulty; // 1-5 scale
+}
