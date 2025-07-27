b80a47f31b123e4dd834a5645cb9e3a6f2e673f3
Shawn Rizo <shawnrizo@hotmail.com>
2025-07-27 13:44:52 -0400

    Feat: Add Contract Title Natspecc & Comments

--- src/MathChallenge1.sol
+++ src/MathChallenge1.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/**
+ * @title MathChallenge
+ * @notice A smart contract that presents mathematical problems for students to solve
+ * @dev This contract allows deployment of individual math problems with tracking of student attempts and solutions
+ * @custom:version 1.0.0
+ */
 contract MathChallenge1 {
     string public problem;
     uint256 public immutable correctAnswer;
