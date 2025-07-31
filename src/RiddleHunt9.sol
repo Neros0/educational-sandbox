ec515d420000f45a9ceda8368364bb86f2e5cf37
Shawn Rizo <shawnrizo@hotmail.com>
2025-07-31 11:23:51 -0400

    Feat: Add attempts mapping

--- src/RiddleHunt9.sol
+++ src/RiddleHunt9.sol
@@ -4,4 +4,6 @@ pragma solidity ^0.8.19;
 contract RiddleHunt9 {
     bytes32 public immutable answerHash;
     string public constant hint = "The answer is a type of food";
+
+    mapping(address => uint256) public attempts;
 }
