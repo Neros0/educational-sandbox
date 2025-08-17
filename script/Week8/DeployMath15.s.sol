2b706b6b51a1090b8a1cd02b47d5336957a42b77
Shawn Rizo <shawnrizo@hotmail.com>
2025-08-17 08:46:18 -0400

    Feat: Reconfigure DeployMath15.s.sol Script

--- script/Week8/DeployMath15.s.sol
+++ script/Week8/DeployMath15.s.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.19;
 
 import {Script, console2} from "forge-std/Script.sol";
-import {MathChallenge7} from "../../src/Week8/MathChallenge7.sol";
+import {MathChallenge15} from "../../src/Week9/MathChallenge15.sol";
 
-contract DeployMath7 is Script {
+contract DeployMath15 is Script {
     function run() external {
         vm.startBroadcast();
 
-        // Deploy the MathChallenge7 contract
-        address mathChallengeContract = address(new MathChallenge7());
+        // Deploy the MathChallenge15 contract
+        address mathChallengeContract = address(new MathChallenge15());
 
-        console2.log("MathChallenge7 deployed at:", mathChallengeContract);
+        console2.log("MathChallenge15 deployed at:", mathChallengeContract);
 
         vm.stopBroadcast();
     }
