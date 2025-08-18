b51bfd5892e83cbbe23a7043f03fc98747a5892a
Shawn Rizo <shawnrizo@hotmail.com>
2025-08-18 00:07:06 -0400

    Feat: Create Deployment Script For 16th Math Challenge

---
+++ script/Week8/DeployMath16.s.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {MathChallenge15} from "../../src/Week9/MathChallenge15.sol";
+
+contract DeployMath15 is Script {}
