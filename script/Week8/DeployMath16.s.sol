a036c66f3e7bdd0670de5b14302daaa363404b30
Shawn Rizo <shawnrizo@hotmail.com>
2025-08-18 00:07:41 -0400

    Feat: Configure DeployMath16.s.sol w/ run Func

--- script/Week8/DeployMath16.s.sol
+++ script/Week8/DeployMath16.s.sol
@@ -4,4 +4,15 @@ pragma solidity ^0.8.19;
 import {Script, console2} from "forge-std/Script.sol";
 import {MathChallenge15} from "../../src/Week9/MathChallenge15.sol";
 
-contract DeployMath15 is Script {}
+contract DeployMath15 is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        // Deploy the MathChallenge15 contract
+        address mathChallengeContract = address(new MathChallenge15());
+
+        console2.log("MathChallenge15 deployed at:", mathChallengeContract);
+
+        vm.stopBroadcast();
+    }
+}
