30015b3a97680a4f55111ca0447a982c4a07a22a
Shawn Rizo <shawnrizo@hotmail.com>
2025-09-25 02:46:31 -0400

    feat: Refactor Code

--- frontend/idea-sandbox/app/context/index.tsx
+++ frontend/idea-sandbox/app/context/index.tsx
@@ -1,47 +1,59 @@
-'use client'
+// app/content/index.tsx
+"use client";
 
-import { wagmiAdapter, projectId } from "@/app/config";
-import { createAppKit } from "@reown/appkit";
-import { mainnet, optimism, base } from "@reown/appkit/networks";
+import { wagmiAdapter, projectId, networks } from "@/app/config";
+import { createAppKit } from "@reown/appkit/react";
+import { base } from "@reown/appkit/networks";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { type ReactNode } from "react";
-import { Context, cookieToInitialState, WagmiProvider, type Config } from "wagmi";
+import { cookieToInitialState, WagmiProvider, type Config } from "wagmi";
 
 const queryClient = new QueryClient();
 
 if (!projectId) {
-    throw new Error("Project Id is not defined");
+    throw new Error("NEXT_PUBLIC_PROJECT_ID is not defined");
 }
 
+// 👇 Always provide metadata for analytics
 const metadata = {
     name: "Idea Sandbox",
     description: "An AI-powered idea generation and brainstorming tool.",
     url: "https://idea-sandbox.vercel.app",
-    icons: ["https://idea-sandbox.vercel.app/favicon.ico"]
-}
+    icons: ["https://idea-sandbox.vercel.app/favicon.ico"],
+};
 
-const modal = createAppKit({
+// 👇 Init AppKit modal + analytics
+createAppKit({
     adapters: [wagmiAdapter],
     projectId,
-    networks: [mainnet, optimism, base],
+    networks: [base, ...networks],
     defaultNetwork: base,
+    metadata,
     features: {
         analytics: true,
         email: true,
-        socials: ['google', 'x', 'github', 'discord', 'farcaster'],
-        emailShowWallets: true
+        socials: ["google", "x", "github", "discord", "farcaster"],
+        emailShowWallets: true,
     },
-    themeMode: 'light'
+    themeMode: "light",
 });
 
-function ContextProvider({ children, cookies }: { children: ReactNode, cookies: string | null }) {
-    const initialState = cookieToInitialState(wagmiAdapter.wagmiConfig as Config, cookies)
+function ContextProvider({
+    children,
+    cookies,
+}: {
+    children: ReactNode;
+    cookies: string | null;
+}) {
+    const initialState = cookieToInitialState(
+        wagmiAdapter.wagmiConfig as Config,
+        cookies
+    );
+
     return (
         <WagmiProvider config={wagmiAdapter.wagmiConfig as Config} initialState={initialState}>
-            <QueryClientProvider client={queryClient}>
-                {children}
-            </QueryClientProvider>
+            <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
         </WagmiProvider>
     );
 }
