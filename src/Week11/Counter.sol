ecdc7619646a535bd44027da7f007e55f090a3a9
Neros0 <shawnrizo@hotmail.com>
2025-10-12 22:43:48 -0400

    feat: Add Base Mainnet Deployments

---
+++ src/Week11/Counter.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Counter {
+    uint256 private count;
+    string private Version = "1.0.2";
+
+    function increment() public {
+        count++;
+    }
+
+    function getCount() public view returns (uint256) {
+        return count;
+    }
+}
