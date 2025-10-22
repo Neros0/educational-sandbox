e86444d9f5b37a9ddad9b92b73fbb9fcdf35692b
Neros0 <shawnrizo@hotmail.com>
2025-10-22 15:22:35 -0400

    feat: Add IReputationRegistry Interface w/ getReputation & getReputationData External View Functions

--- src/Week11/UserProfile.sol
+++ src/Week11/UserProfile.sol
@@ -5,4 +5,19 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 
+interface IReputationRegistry {
+    function getReputation(address user) external view returns (uint256);
+    function getReputationData(address user)
+        external
+        view
+        returns (
+            uint256 score,
+            uint256 totalRatings,
+            uint256 totalScore,
+            uint256 lastUpdateTime,
+            uint256 lastDecayTime,
+            bool isRegistered
+        );
+}
+
 contract UserProfile {}
