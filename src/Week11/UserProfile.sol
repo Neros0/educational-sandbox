7fbe77056d60b2065a683c160af24c312a6c2f28
Neros0 <shawnrizo@hotmail.com>
2025-10-23 14:49:14 -0400

    feat: Add Badge management

--- src/Week11/UserProfile.sol
+++ src/Week11/UserProfile.sol
@@ -142,6 +142,10 @@ contract UserProfile is Ownable, ReentrancyGuard, Pausable {
     mapping(address => bool) public isRegistered;
     mapping(address => uint256) public registrationIndex;
 
+    // Badge management
+    mapping(BadgeType => bool) public availableBadges;
+    mapping(address => bool) public badgeIssuers;
+
     constructor(address _reputationRegistry, address _ratingSystem, address _owner) Ownable(_owner) {
         reputationRegistry = IReputationRegistry(_reputationRegistry);
         ratingSystem = IRatingSystem(_ratingSystem);
