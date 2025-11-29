4de76335b9b05200751f8cd00aad82da1d0e4447
Neros0 <shawnrizo@hotmail.com>
2025-11-29 02:44:52 -0500

    feat: Add Constructor Function

--- src/Week11/LPToken.sol
+++ src/Week11/LPToken.sol
@@ -9,4 +9,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  * @notice ERC20 token representing shares in the lending pool
  * @dev Only the lending pool contract can mint and burn tokens
  */
-contract LPToken is ERC20, Ownable {}
+contract LPToken is ERC20, Ownable {
+    constructor() ERC20("Lending Pool Token", "LPT") Ownable(msg.sender) {}
+}
