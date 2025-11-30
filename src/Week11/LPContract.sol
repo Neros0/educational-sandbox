bc8039e1aba45e3d2936becd27ab09f6a9e09a1d
Neros0 <shawnrizo@hotmail.com>
2025-11-30 14:06:48 -0500

    feat: Add Imports, Inheritance and Contract Title Natspec Comments

--- src/Week11/LPContract.sol
+++ src/Week11/LPContract.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "./ILPContract.sol";
+import "./LPToken.sol";
+
+/**
+ * @title LPContract
+ * @notice A simple lending pool where users can deposit tokens to earn yield and borrow against collateral
+ * @dev Implements a basic lending/borrowing mechanism with interest accrual
+ */
+contract LPContract is ILPContract, Ownable, ReentrancyGuard {}
