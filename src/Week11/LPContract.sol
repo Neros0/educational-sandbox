cd0e42f87b29633c74e73e65b4451285a0ef21a7
Neros0 <shawnrizo@hotmail.com>
2025-12-01 20:44:20 -0500

    feat: Add withdrawCollateral External Function Natspec Comments

--- src/Week11/LPContract.sol
+++ src/Week11/LPContract.sol
@@ -136,6 +136,10 @@ contract LPContract is ILPContract, Ownable, ReentrancyGuard {
         emit DepositCollateral(msg.sender, amount);
     }
 
+    /**
+     * @notice Withdraws collateral if health factor allows
+     * @param amount Amount of collateral to withdraw
+     */
     function withdrawCollateral(uint256 amount) external nonReentrant {
         require(amount > 0, "Amount must be greater than 0");
         require(userInfo[msg.sender].collateralBalance >= amount, "Insufficient collateral");
