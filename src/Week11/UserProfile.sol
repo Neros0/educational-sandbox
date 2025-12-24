14190053045979f45ec0e684163c35d29efd8525
Neros0 <shawnrizo@hotmail.com>
2025-12-24 17:19:53 -0500

    feat: Add error InvalidBioLength

--- src/Week11/UserProfile.sol
+++ src/Week11/UserProfile.sol
@@ -172,6 +172,7 @@ contract UserProfile is Ownable, ReentrancyGuard, Pausable {
     error UserNotRegistered(address user);
     error UsernameAlreadyTaken(string username);
     error InvalidUsername(string username);
+    error InvalidBioLength(uint256 length);
 
     constructor(address _reputationRegistry, address _ratingSystem, address _owner) Ownable(_owner) {
         reputationRegistry = IReputationRegistry(_reputationRegistry);
