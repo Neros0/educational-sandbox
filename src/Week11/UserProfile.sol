643fa2bd149d6acede3dbc7f71cd7999c395a90a
Neros0 <shawnrizo@hotmail.com>
2025-12-24 17:19:14 -0500

    feat: Add error InvalidUsername

--- src/Week11/UserProfile.sol
+++ src/Week11/UserProfile.sol
@@ -171,6 +171,7 @@ contract UserProfile is Ownable, ReentrancyGuard, Pausable {
     error UserAlreadyRegistered(address user);
     error UserNotRegistered(address user);
     error UsernameAlreadyTaken(string username);
+    error InvalidUsername(string username);
 
     constructor(address _reputationRegistry, address _ratingSystem, address _owner) Ownable(_owner) {
         reputationRegistry = IReputationRegistry(_reputationRegistry);
