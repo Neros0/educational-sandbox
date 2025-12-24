31c56e8cda9948bf91139e4a25ddacb303cc0f9b
Neros0 <shawnrizo@hotmail.com>
2025-12-24 17:18:53 -0500

    feat: Add error UsernameAlreadyTaken

--- src/Week11/UserProfile.sol
+++ src/Week11/UserProfile.sol
@@ -170,6 +170,7 @@ contract UserProfile is Ownable, ReentrancyGuard, Pausable {
     // Errors
     error UserAlreadyRegistered(address user);
     error UserNotRegistered(address user);
+    error UsernameAlreadyTaken(string username);
 
     constructor(address _reputationRegistry, address _ratingSystem, address _owner) Ownable(_owner) {
         reputationRegistry = IReputationRegistry(_reputationRegistry);
