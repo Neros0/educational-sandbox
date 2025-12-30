6863f3ee9fc038181eab97b3618a973295ada329
Neros0 <shawnrizo@hotmail.com>
2025-12-29 22:32:34 -0500

    feat: Add error ProfileNotPublic

--- src/Week11/UserProfile.sol
+++ src/Week11/UserProfile.sol
@@ -180,6 +180,7 @@ contract UserProfile is Ownable, ReentrancyGuard, Pausable {
     error UserIsBlocked();
     error InvalidBadgeType();
     error UnauthorizedBadgeIssuer();
+    error ProfileNotPublic();
 
     constructor(address _reputationRegistry, address _ratingSystem, address _owner) Ownable(_owner) {
         reputationRegistry = IReputationRegistry(_reputationRegistry);
