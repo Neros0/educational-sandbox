d1fd461143914c5d94ccfaf558b34dab6f7e878f
Neros0 <shawnrizo@hotmail.com>
2025-12-30 20:38:55 -0500

    feat: Add error NotBlocked

--- src/Week11/UserProfile.sol
+++ src/Week11/UserProfile.sol
@@ -185,6 +185,7 @@ contract UserProfile is Ownable, ReentrancyGuard, Pausable {
     error AlreadyFollowing();
     error NotFollowing();
     error AlreadyBlocked();
+    error NotBlocked();
 
     constructor(address _reputationRegistry, address _ratingSystem, address _owner) Ownable(_owner) {
         reputationRegistry = IReputationRegistry(_reputationRegistry);
